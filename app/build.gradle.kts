bf3796152125fcc7f3048cb902dbbd2c97716ebf
negar <negar.271999@gmail.com>
2024-11-05 14:00:18 -0800

    fix ci 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.scotiabankproject"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.scotiabankproject"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
